15becdf96f10bc0aaf2c51f2a9d58f9411fa08c1
inhakki <inhakki@gmail.com>
2015-01-27 16:23:13 -0500

    Ember set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'assets/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root :to => 'assets#index'
 
   resources :users, only: [:create]
   resources :sessions, only: [:create, :destroy]
